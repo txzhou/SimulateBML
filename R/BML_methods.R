06c424151730a54af287d2845de77a91dfb7dcd1
Tianxia Zhou <tianxiazhou@gmail.com>
2016-10-08 14:14:16 -0700

    Revert "all R package."
    This reverts commit 55ca706624d59b367bdcaede4bc8781abc41bc8e.

--- R/BML_methods.R
+++ R/BML_methods.R
@@ -6,14 +6,15 @@ summary.BML = function(object, ...) {
   BML.Grid = object
   s = (table(BML.Grid))
   return(list(c(num.cells = sum(s),
-                num.red = s["red"][[1]],
-                num.blue = s["blue"][[1]]),
-              density = (s["red"][[1]] +s["blue"][[1]])/sum(s),
+                num.red = s["1"][[1]],
+                num.blue = s["2"][[1]]),
+              density = (s["1"][[1]] +s["2"][[1]])/sum(s),
               velocity = velocityBMLGrid(BML.Grid)))
 }
 
 plot.BML = function(x, y, palette = c(red = "#ca0020", blue = "#92c5de", white = "#f7f7f7"), title = "", ...) {
   BML.Grid = x
+  color = as.character(factor(BML.Grid, labels = c("white", "red", "blue")))
   x_right = ncol(BML.Grid)+0.5
   y_top = nrow(BML.Grid)+0.5
   # Graphical display
@@ -27,7 +28,7 @@ plot.BML = function(x, y, palette = c(red = "#ca0020", blue = "#92c5de", white =
        ybottom = as.vector(row(BML.Grid)-0.5),
        xright = as.vector(col(BML.Grid)+0.5),
        ytop = as.vector(row(BML.Grid)+0.5),
-       col = palette[BML.Grid],
+       col = palette[color],
        border = "transparent")
   # print a outer boarder
   rect(xleft = 0.5,
